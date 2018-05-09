38b89fecb0a952a02f62280b06db1719bb974e9d
Jefwillems <jefwillems100@gmail.com>
2018-05-09 15:16:59 +0200

    add verbose mode

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const fetch = require("node-fetch");
 
 router.use("/", (req, res, next) => {
+  const verbose = req.query["v"];
+  console.log(verbose);
   fetch(
     "https://datatank.stad.gent/4/cultuursportvrijetijd/gentsefeestenlocaties.json"
   )
@@ -10,10 +12,12 @@ router.use("/", (req, res, next) => {
     })
     .then(json => {
       json = [json[0]];
-      json = json.map(el => {
-        let { name, address, outDoors } = el;
-        return { name, address, outDoors };
-      });
+      if (!verbose) {
+        json = json.map(el => {
+          let { name, address, outDoors } = el;
+          return { name, address, outDoors };
+        });
+      }
       return res.json(json);
     })
     .catch(next);
