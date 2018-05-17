5a0570e5393f850c2e114ba63d88d76b57027429
Jefwillems <jefwillems100@gmail.com>
2018-05-17 16:51:38 +0200

    add quick replies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const _ = require("../util/util");
 
-/**
- *
- */
 router.get("/", (req, res, next) => {
   _.fetchPointsOfInterest()
     .then(json => {
@@ -16,21 +13,57 @@ router.get("/", (req, res, next) => {
     .catch(next);
 });
 
+const t = {
+  messages: [
+    {
+      text: "",
+      quick_replies: [
+        // {
+        //   set_attributes: {
+        //     "some attribute": "some value",
+        //     "another attribute": "another value"
+        //   },
+        //   block_names: ["Block 1"],
+        //   type: "show_block",
+        //   title: "Go!"
+        // },
+        // {
+        //   set_attributes: {
+        //     "some attribute": "some value",
+        //     "another attribute": "another value"
+        //   },
+        //   block_names: ["Block 1"],
+        //   type: "show_block",
+        //   title: "Go!"
+        // }
+      ]
+    }
+  ]
+};
+
 router.get("/places", (req, res, next) => {
   _.fetchPointsOfInterest().then(json => {
     json = _.combineUrls(json);
     json = json.map(el => {
       return _.filterProperties(el);
     });
-    let response = Object.create(null);
-    response["messages"] = [];
-    response.messages.push({
-      text: req.t("poi_find")
-    });
+    let response = {};
+    Object.assign(response, t);
+    response.messages[0].text = req.t("poi_find");
+    let message = response.messages[0];
     json.forEach(poi => {
-      response.messages.push({
-        text: poi.name[req.loc][0]
-      });
+      let btn = {
+        title: poi.name[req.loc][0],
+        type: "show_block",
+        block_names: ["GET_POIdescription"],
+        set_attributes: {
+          POI: poi["@id"]
+        }
+      };
+      if (!message.quick_replies) {
+        message.quick_replies = [];
+      }
+      message.quick_replies.push(btn);
     });
     res.json(response);
   });
