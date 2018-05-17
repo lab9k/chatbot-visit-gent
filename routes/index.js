97770781f17108a7f4c7c171d20372114a838d44
Jefwillems <jefwillems100@gmail.com>
2018-05-17 12:03:14 +0200

    combine urls

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,46 @@
 const router = require("express").Router();
 const fetch = require("node-fetch");
 
+/**
+ *
+ */
 router.use("/", (req, res, next) => {
-  const nonverbose = req.query["v"];
-  fetch(
-    "https://datatank.stad.gent/4/cultuursportvrijetijd/gentsefeestenlocaties.json"
-  )
+  fetch("https://visit.gent.be/en/lod/poi")
     .then(data => {
       return data.json();
     })
     .then(json => {
-      if (nonverbose) {
-        json = json.map(el => {
-          delete el["@context"];
-          return el;
-        });
-      }
+      json = combineUrls(json);
       return res.json(json);
     })
     .catch(next);
 });
+
+const combineUrls = json => {
+  let combinedLang = [];
+  json.forEach(el => {
+    let exists = combinedLang.find(i => {
+      return i["@id"] === el["@id"];
+    });
+    if (exists) {
+      let lang = findLang(el.url);
+      exists.url[lang] = [el["url"]];
+    } else {
+      let lang = findLang(el["url"]);
+      let urlField = {};
+      urlField[lang] = [el["url"]];
+      let newEl = Object.assign({}, el);
+      newEl.url = urlField;
+      combinedLang.push(newEl);
+    }
+  });
+  return combinedLang;
+};
+
+const findLang = url => {
+  let pattern = /visit.gent.be\/([a-z]{2})\//g;
+  let match = pattern.exec(url);
+  return match[1];
+};
 
 exports = module.exports = router;
