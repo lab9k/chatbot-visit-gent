376159af579f4e2e54e5983aff7829bdcd407753
Jefwillems <jefwillems100@gmail.com>
2018-06-13 18:15:05 +0200

    logging

--- routes/gf/index.js
+++ routes/gf/index.js
@@ -32,7 +32,7 @@ router.all('/', mw.typeMiddleware, (req, res, next) => {
 const handleLocation = (req, res /* , next */) => {
   const original = req.body.originalDetectIntentRequest;
   const { payload } = original;
-  console.log(payload);
+  console.log(JSON.stringify(payload));
   const { lat, long } = payload.data.postback.data;
   const squares = locationMapper.getSquares();
   const nearest = loc.closestLocation({ lat, long }, squares);
