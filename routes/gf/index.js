570b5d7771f245bbad9ab90e08eea94c00686968
Ruben Alliet <allietruben@gmail.com>
2018-06-20 14:15:47 +0200

    test button

--- routes/gf/index.js
+++ routes/gf/index.js
@@ -276,7 +276,7 @@ const getPleinCard = (req, res /* , next */) => {
       ),
       new CardButton(
         'Programma',
-        `Programma ${square.name.nl}`,
+        `Programma ${square.display_name}`,
         'postback'
     )
     ]
