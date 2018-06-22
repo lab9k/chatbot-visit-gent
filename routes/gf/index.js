ae72a175d19766b46861852ea030c07e75ae0ee6
Ruben Alliet <allietruben@gmail.com>
2018-06-22 11:33:59 +0200

    test button

--- routes/gf/index.js
+++ routes/gf/index.js
@@ -135,7 +135,7 @@ const searchToiletten = (req, res) => {
 
   const quickReply = new QuickReply();
 
-  console.log(quickReply.getResponse())
+  //console.log(quickReply.getResponse())
 
   const ret = {
     payload: {
@@ -144,7 +144,10 @@ const searchToiletten = (req, res) => {
           type: 'template',
           payload: {
             template_type: 'generic',
-            elements: [card.getResponse()]
+            elements: [
+              card.getResponse(),
+              quickReply.getResponse()
+            ]
           }
         }
       }
