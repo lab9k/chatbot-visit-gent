90c9b9754b37ee6410a214f9cfa125fd9eab3256
Ruben Alliet <allietruben@gmail.com>
2018-06-29 16:57:55 +0200

    add default image_url

--- routes/gf/index.js
+++ routes/gf/index.js
@@ -147,10 +147,16 @@ const handleEvents = (req, res) => {
       return console.log(err);
       //list to store all cards of events
       let cardList = [];
+
+      
       //console.log(events)
       events.forEach((event) => {
           //const square = locationMapper.getSquares().find(square => square.name.nl.toLowerCase() == event.address.toLowerCase());
           // construct a Card object for each event
+          if(event.image_url == null) {
+            event.image_url = "https://www.uitinvlaanderen.be/sites/default/files/styles/large/public/beeld_gf_nieuwsbericht.jpg"
+          }
+
           const card = new Card(
             `${event.image_url}`,
             `${event.name} (${moment(event.startDate).format('H:mm')} - ${moment(event.endDate).format('H:mm')})`, [0,3], {
