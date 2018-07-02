8a0f4ee532f95efe449c4016c1b42c413b4a1081
Ruben Alliet <allietruben@gmail.com>
2018-07-02 14:07:55 +0200

    to string

--- db/cosmosDB/cosmosDBManager.js
+++ db/cosmosDB/cosmosDBManager.js
@@ -49,8 +49,8 @@ const getEventsSelectedStageAndDate = (dateTimeStart, stageName) => {
     );
 
 
-    const startDate = moment(dateTimeStart).format('YYYY-MM-DD[T]HH:mm:ss');
-    const endDate = moment(dateTimeStart).add(1, 'day').format('YYYY-MM-DD[T]HH:mm:ss');
+    const startDate = moment(dateTimeStart.toString()).format('YYYY-MM-DD[T]HH:mm:ss');
+    const endDate = moment(dateTimeStart.toString()).add(1, 'day').format('YYYY-MM-DD[T]HH:mm:ss');
 
 
     console.log("start:",startDate, typeof(startDate))
