868fc545505903a1ff2b524782b8a6f1e2c60098
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-03 18:36:32 +0200

    test get event now

--- routes/gf/index.js
+++ routes/gf/index.js
@@ -418,8 +418,7 @@ const getEventsGentseFeestenNow = (req, res /* , next */ ) => {
       //reject(err),/*
     if (err)
       return console.log(err);
-    return events;
-  }).then(events => {
+    
     if (events.length == 0) {
       const defaultMenu = ["Feestpleinen","Toilet","Feedback"]
       const quickReply = new QuickReply("Er zijn op dit moment geen evenementen op de Gentse Feesten, Hoe kan ik je verder helpen?", defaultMenu).getResponse();
@@ -484,9 +483,9 @@ const getEventsGentseFeestenNow = (req, res /* , next */ ) => {
       }
     };
     return res.json(payload);
-  }).catch(function(e){
+  })/*.catch(function(e){
     console.log(err);
-  });
+  });*/
 }
 
 
