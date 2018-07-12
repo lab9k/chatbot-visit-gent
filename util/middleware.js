90477c0770ca8f4f9000a93cf6d28ff083e1d89f
Michiel Derveeuw <mderveeuw@gmail.com>
2018-07-12 12:25:32 +0200

    Remove unused code

--- util/middleware.js
+++ util/middleware.js
@@ -1,38 +1,3 @@
-const idMiddleWare = (req, res, next) => {
-  if (!req.query.id) return res.json({ error: 'id url was not specified' });
-  req.poi_id = req.query.id;
-  return next();
-};
-
-const langMiddleWare = (req, res, next) => {
-  const message = req.body.incoming_message;
-  if (!message) return res.json({ error: 'something went wrong' });
-  switch (message.toLowerCase()) {
-    case 'nederlands':
-      req.loc = 'nl';
-      break;
-    case 'english':
-      req.loc = 'en';
-      break;
-    case 'français' || 'francais':
-      req.loc = 'fr';
-      break;
-    case 'deutsch':
-      req.loc = 'de';
-      break;
-    case 'español' || 'espanol':
-      req.loc = 'es';
-      break;
-    default:
-      req.loc = 'en';
-      req.error = 'Language was not found, defaulting to English';
-      break;
-  }
-  return next();
-};
-
-// req.body.queryResult.action
-
 const typeMiddleware = (req, res, next) => {
   const qResult = req.body.queryResult;
   if (qResult) {
@@ -77,7 +42,5 @@ const typeMiddleware = (req, res, next) => {
 };
 
 module.exports = {
-  idMiddleWare,
-  langMiddleWare,
   typeMiddleware,
 };
