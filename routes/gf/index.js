333304634fb53b5e422c0c6278cbf7d29624b2b1
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-13 01:00:51 +0200

    fix catch

--- routes/gf/index.js
+++ routes/gf/index.js
@@ -475,9 +475,9 @@ const getEventsGentseFeestenNow = (req, res /* , next */ ) => {
       }
     };
     return res.json(payload);
-  }).catch(err){
+  }).catch((err) => {
     console.log("error events now", err);
-  };
+  });
 };
 
 router.get('/debug', (req, res) => {
