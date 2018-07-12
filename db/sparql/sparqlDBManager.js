6d3e003fa504f7adfe8100d3be9b5de89d5be26e
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-07-12 16:31:19 +0200

    fix query

--- db/sparql/sparqlDBManager.js
+++ db/sparql/sparqlDBManager.js
@@ -18,13 +18,13 @@ const getAllEventsFromNow = () => {
   const date = moment(new Date('2018-07-18'))
     .set('hour', 9)
     .set('minute', 0)
-    .format('YYYY-MM-DDTHH:mm')
+    .format('YYYY-MM-DD[T]HH:mm')
     .toString();
   const endDate = moment(date)
     .add(1, 'day')
     .set('hour', 5)
     .set('minute', 0)
-    .format('YYYY-MM-DDTHH:mm')
+    .format('YYYY-MM-DD[T]HH:mm')
     .toString();
 
   console.log('date:', date);
@@ -62,12 +62,13 @@ const getEventsSelectedStageAndDate = (stageName, date) => {
   const convertedDate = moment(date)
     .format('YYYY-MM-DD')
     .toString();
-    const day = date.getDate();
+  const startDay = date.getDate();
+  const endDay = date.getDate()+1;
   console.log('converted date', convertedDate);
 
   console.log("day", day);
 
-  const q = SPARQL`PREFIX schema: <http://schema.org/>
+  const q = `PREFIX schema: <http://schema.org/>
   PREFIX dct:<http://purl.org/dc/terms/>
   SELECT ?name ?startDate ?location from <http://stad.gent/gentse-feesten-2018/> WHERE {
       ?sub a <http://schema.org/Event> .
@@ -80,8 +81,8 @@ const getEventsSelectedStageAndDate = (stageName, date) => {
       UNION {
           ?sub schema:location/schema:containedInPlace/schema:name ?location
       }
-      FILTER (?startDate > "2018-07-${date.getDate()}T09:00+02:00"^^xsd:dateTime ).
-      FILTER (?endDate < "2018-07-${date.getDate() + 1}T05:00+02:00"^^xsd:dateTime ).
+      FILTER (?startDate > "2018-07-${startDay}T09:00+02:00"^^xsd:dateTime ).
+      FILTER (?endDate < "2018-07-${endDay}T05:00+02:00"^^xsd:dateTime ).
       FILTER contains(?location, ${stageName}).
   }`;
 
