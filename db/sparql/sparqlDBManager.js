2f245dc2a9459c473ddbe99a2904996eb4ed7768
Jefwillems <jefwillems100@gmail.com>
2018-07-13 14:17:49 +0200

    style, bugfix

--- db/sparql/sparqlDBManager.js
+++ db/sparql/sparqlDBManager.js
@@ -1,41 +1,41 @@
-const { SparqlClient, SPARQL } = require("sparql-client-2");
+const { SparqlClient } = require('sparql-client-2');
 
-const moment = require("moment");
+const moment = require('moment');
 
-const endpoint = "https://stad.gent/sparql";
-const client = new SparqlClient("https://stad.gent/sparql").register({
-  db: "http://stad.gent/gentse-feesten-2018/",
-  dct: "http://purl.org/dc/terms/",
-  schema: "http://schema.org/",
-  startdate: "http://schema.org/startDate"
+const endpoint = 'https://stad.gent/sparql';
+const client = new SparqlClient(endpoint).register({
+  db: 'http://stad.gent/gentse-feesten-2018/',
+  dct: 'http://purl.org/dc/terms/',
+  schema: 'http://schema.org/',
+  startdate: 'http://schema.org/startDate'
 });
 
-const getAllEventsFromNow = square => {
-  //console.log('test events now');
-  let shortDate = new Date("2018-07-13T17:00+02:00");
+const getAllEventsFromNow = (square) => {
+  // console.log('test events now');
+  const shortDate = new Date('2018-07-13T17:00+02:00');
   const date = moment
     .parseZone(shortDate)
-    .format("YYYY-MM-DD[T]HH:mm[+02:00]")
+    .format('YYYY-MM-DD[T]HH:mm[+02:00]')
     .toString();
   let endDate = moment
     .parseZone(date)
-    .set("hours", 6)
-    .set("minutes", 0);
+    .set('hours', 6)
+    .set('minutes', 0);
 
-    //als het nog voor middernacht is, moeten events van na middernacht ook getoond worden
+  // als het nog voor middernacht is, moeten events van na middernacht ook getoond worden
   if (endDate < date) {
-    var tomorrow = new Date(shortDate);
+    let tomorrow = new Date(shortDate);
     tomorrow.setDate(tomorrow.getDate() + 1);
     tomorrow = moment
-    .parseZone(tomorrow)
-    .set("hours", 6)
-    .set("minutes", 0);
+      .parseZone(tomorrow)
+      .set('hours', 6)
+      .set('minutes', 0);
   }
 
-  endDate = endDate.format("YYYY-MM-DD[T]HH:mm[+02:00]").toString();
+  endDate = endDate.format('YYYY-MM-DD[T]HH:mm[+02:00]').toString();
 
-  //console.log('date:', date);
-  const squareFilter = square ? `FILTER contains(?location, "${square}").` : "";
+  // console.log('date:', date);
+  const squareFilter = square ? `FILTER contains(?location, "${square}").` : '';
 
   const q = `SELECT ?name ?startDate ?endDate ?image ?location ?description from <http://stad.gent/gentse-feesten-2018/> WHERE {
     ?sub a <http://schema.org/Event> .
@@ -48,8 +48,8 @@ const getAllEventsFromNow = square => {
         ?sub schema:location/schema:name ?location
     }
     UNION {
-        ?sub schema:location/schema:containedInPlace/schema:name ?location.   
-    		?sub schema:location/schema:additionalType ?additionalType .
+        ?sub schema:location/schema:containedInPlace/schema:name ?location.
+        ?sub schema:location/schema:additionalType ?additionalType .
     }
     FILTER (?startDate > "${date}"^^xsd:dateTime ).
     FILTER (?endDate < "${endDate}"^^xsd:dateTime ).
@@ -59,26 +59,27 @@ const getAllEventsFromNow = square => {
   order by ?startDate
 `;
 
-  console.log("query events now", q);
+  console.log('query events now', q);
 
   return client
     .query(q)
     .execute()
-    .catch(error => {
-      console.log("sparql error", error);
+    .catch((error) => {
+      console.log('sparql error', error);
     });
 };
 
 const getEventsSelectedStageAndDate = (square, date) => {
-  //console.log('converted date issue', date);
-  date = new Date(date);
+  // console.log('converted date issue', date);
+  // date = new Date(date);
+  const nDate = new Date(date);
   const convertedDate = moment
-    .parseZone(date)
-    .format("YYYY-MM-DD")
+    .parseZone(nDate)
+    .format('YYYY-MM-DD')
     .toString();
-  const startDay = date.getDate();
-  const endDay = date.getDate() + 1;
-  console.log('db date', date);
+  const startDay = nDate.getDate();
+  const endDay = nDate.getDate() + 1;
+  console.log('db date', nDate);
 
   console.log('day', startDay);
 
@@ -93,7 +94,7 @@ const getEventsSelectedStageAndDate = (square, date) => {
         ?sub schema:location/schema:name ?location
     }
     UNION {
-        ?sub schema:location/schema:containedInPlace/schema:name ?location .     
+        ?sub schema:location/schema:containedInPlace/schema:name ?location .
     }
     FILTER (?startDate > "${convertedDate}T09:00+02:00"^^xsd:dateTime ).
     FILTER (?endDate < "2018-07-${endDay}T05:00+02:00"^^xsd:dateTime ).
@@ -102,13 +103,13 @@ const getEventsSelectedStageAndDate = (square, date) => {
   order by ?startDate
   `;
 
-  //console.log(q);
+  // console.log(q);
 
   return client
     .query(q)
     .execute()
-    .catch(error => {
-      console.log("sparQL error", JSON.stringify(error));
+    .catch((error) => {
+      console.log('sparQL error', JSON.stringify(error));
     });
 };
 
